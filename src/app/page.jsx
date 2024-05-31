c7ffbc8b9ba7a64fc7104b7dbc17bcb7b7168a6b
al-mojtaba <mojtabadric@gmail.com>
2024-05-31 07:37:27 +0300

    fixed commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,7 +13,7 @@ export default function page() {
       />
 
       <div className="w-[400px] md:w-[450px] lg:w-[500px] text-center">
-        <p className="text-5xl  mb-8">I'm <span className=" text-amber-500">Mohamed</span></p>
+        <p className="text-5xl  mb-8">I&apos;m <span className=" text-amber-500">Mohamed</span></p>
         
         <p className="text-2xl">and I am thrilled to have you visit my online space. This website is dedicated to sharing my journey, passions, and the various projects I am involved in.</p>
       </div>
